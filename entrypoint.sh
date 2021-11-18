4518429080f499d22c9f4d21f9a5d79a6b656d80
samir <samir@incubaid.com>
2021-11-18 12:33:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Create dirs for electrum and jsmodel if not created, create links for them in the volume
-mkdir -p ~/.config/jumpscale/secureconfig/jumpscale/packages
-mkdir -p /data/jsngmodel && ln -s /data/jsngmodel ~/.config/jumpscale/secureconfig/jumpscale/packages/gettft
-mkdir -p /data/electrum && ln -s /data/electrum ~/.electrum
-
 # Edit the default domain with the passed one
-sed -i "s/domain = \"gettft.grid.tf\"/domain = \"$domain\"/g" /code/tftshop/jumpscale/packages/gettft/package.toml 
+sed -i "s/domain = \"gettft.grid.tf\"/domain = \"$domain\"/g" ~/code/tftshop/jumpscale/packages/gettft/package.toml 
 
 # Create a dummy identity to start jsng with Threefold Connect
 # Moved to entrypoint to save all data in the mounted volumes and avoid to override
@@ -18,7 +13,7 @@ poetry run jsng 'j.clients.stellar.new("test",network="TEST"); j.clients.stellar
 
 # Create the default threebot
 poetry run jsng 'j.servers.threebot.new("default"); j.servers.threebot.default.save()'
-poetry run jsng "j.servers.threebot.default.packages.add(path='/code/tftshop/jumpscale/packages/gettft')"
+poetry run jsng "j.servers.threebot.default.packages.add(path='/home/gitpod/code/tftshop/jumpscale/packages/gettft')"
 
 # Start electrum daemon
 if [ $network == "testnet" ]; then
@@ -32,6 +27,3 @@ fi
 # Configure tftshop mpk and wallet
 poetry run jsng 'j.core.config.set("tftshop_mpk", os.environ.get("mpk"))'
 poetry run jsng "wallet=j.clients.stellar.get(\"tftshop_wallet\"); wallet.secret=\"$tftshop_wallet_secret\"; wallet.network=\"$stellar_network\"; wallet.save()"
-
-# Start threebot server without certificate as kubernetes manages it
-poetry run threebot start --no-cert --local --background
